e0f67ae58d80ea450b0dec0bf8bf697c59e7bfc8
9z0b3t1c <9z0b3t1c@protonmail.com>
2022-06-12 22:02:47 +0100

    Adding healthcheck endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpRequest, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
