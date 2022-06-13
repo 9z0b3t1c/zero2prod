5ef499dc8b0a4d3d77badc948408982d155a8198
9z0b3t1c <9z0b3t1c@protonmail.com>
2022-06-13 11:47:56 +0100

    Formatting.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8000")
-        .expect("Failed to bind to random port");
+    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to bind to random port");
     run(listener)?.await
 }
